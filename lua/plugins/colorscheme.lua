dad6e2b7149fb672e00edceb465dfc37f8ac6001
Suvigya Saxena <suvigya2001@gmail.com>
2025-11-08 11:59:53 +0530

    fix(colorscheme): fixed rose pine colorscheme lag

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,5 @@
 function ColorMyPencils(color)
-  color = color or "rose-pine"
+  color = color or "rose-pine-moon"
   vim.cmd.colorscheme(color)
 
   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
@@ -59,7 +59,7 @@ return {
       require("rose-pine").setup({
         disable_background = true,
         styles = {
-          bold = true,
+          bold = false,
           italic = false,
           transparency = true,
         },
